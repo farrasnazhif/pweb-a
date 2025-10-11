372fe410bbf35f2fc1370e476accc3ec805a46ca
Farras Nazhif <farrasnazhif@gmail.com>
2025-10-11 12:53:17 +0700

    Add files via upload

---
+++ ajax-form/ajax.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    echo "Thank you for contacting us, $name. We will get back to you shortly.";
+}
+?>
